adbe5423456a165bf3ff275b769d8e209696060e
Kimiblock <kimiblock@icloud.com>
2023-10-14 13:46:36 +0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+function getList(){
+	lines=$(cat sub.list | wc -l)
+	lines=$(expr ${lines} + 2)
+	l=1
+	while [[ ${l} -lt ${lines} ]]; do
+		url=$(cat sub.list | head -n ${l} | tail -n 1)
+		l=$(expr ${l} + 1)
+		echo "Downloading ${url}"
+		curl "${url}" -o /tmp/domainBlock-${RANDOM}.txt -L
+	done
+}
+
+function mergeList(){
+	cat /tmp/domainBlock-*.txt >/tmp/domainBlock.txt
+	sed -i '$!N; /^\(.*\)\n\1$/!P; D' /tmp/domainBlock.txt
+	cp /tmp/domainBlock.txt .
+	#rm /tmp/domainBlock*.txt
+}
+
+getList
+mergeList
